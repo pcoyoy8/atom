d0bae495fe0163a30b6535ea2f8c5ebbf73a9646
Pablo Coyoy <pablo@oaky.com>
2022-12-12 15:34:27 +0100

    feat: support custom characters
    - custom separator
    - custom undefined value
    - custom null value

--- src/helpers/csv.ts
+++ src/helpers/csv.ts
@@ -1,10 +1,23 @@
-export function json2Csv(columns: string[], data: object[]): string {
-  let csv = [...columns].map((column) => `"${column}"`).join();
+export interface json2CsvDefaultValues {
+  undefined?: string,
+  null?: string,
+}
 
-  const separator = (index: number): string => (index ? "," : "");
+export function json2Csv(columns: string[], data: object[], separatorString: string = ',', defaultValues? :json2CsvDefaultValues): string {
+  let csv = [...columns].map((column) => `"${column}"`).join(separatorString);
+
+  const separator = (index: number): string => (index ? separatorString : '');
   const getCellValue = (value: any): string => {
-    if (value === undefined) return "";
-    return typeof value === "string" ? `"${value}"` : value;
+    if (value === undefined) {
+      if (defaultValues?.undefined !== undefined) return defaultValues.undefined;
+      return '';
+    }
+
+    if (value === null && defaultValues?.null !== undefined) {
+      return defaultValues.null;
+    }
+
+    return typeof value === 'string' ? `"${value}"` : value;
   };
 
   data.forEach((item) => {
